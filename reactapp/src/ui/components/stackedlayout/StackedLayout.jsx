26e279ddbe92624bb674bb55f58671a5aab8f5a9
Samuel Maciel <samuelmacielsts@gmail.com>
2024-06-24 14:36:02 -0300

    Fix: Folder img

--- reactapp/src/ui/components/stackedlayout/StackedLayout.jsx
+++ reactapp/src/ui/components/stackedlayout/StackedLayout.jsx
@@ -59,7 +59,7 @@ export default function StackedLayout() {
                                             <a href="/home">
                                                 <img
                                                     className="h-8 w-8"
-                                                    src="../public/online-shop-ecommerce-svgrepo-com.png"
+                                                    src="../dist/online-shop-ecommerce-svgrepo-com.png"
                                                     alt="Logo"
                                                 />
                                             </a>
@@ -107,7 +107,7 @@ export default function StackedLayout() {
                                                     <MenuButton className="relative flex max-w-xs items-center rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
                                                         <span className="absolute -inset-1.5" />
                                                         <span className="sr-only">Open user menu</span>
-                                                        <img className="h-8 w-8 rounded-full" src="../public/avatar-boy-svgrepo-com.png" alt="avatar" />
+                                                        <img className="h-8 w-8 rounded-full" src="../dist/avatar-boy-svgrepo-com.png" alt="avatar" />
                                                     </MenuButton>
                                                 </div>
                                                 <Transition
