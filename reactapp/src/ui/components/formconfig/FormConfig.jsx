149360d611b3d7f8ff06769d68598e6409bafb15
Samuel Maciel <samuelmacielsts@gmail.com>
2024-06-24 18:08:39 -0300

    Feat: Form config

--- reactapp/src/ui/components/formconfig/FormConfig.jsx
+++ reactapp/src/ui/components/formconfig/FormConfig.jsx
@@ -1,13 +1,21 @@
 import { PhotoIcon, UserCircleIcon } from '@heroicons/react/24/solid'
+import { useNavigate } from 'react-router-dom';
 
 export default function FormConfig() {
+    const navigate = useNavigate();
+
+    const handleClickCancel = () => {
+        navigate('/home');
+    };
+
     return (
         <form>
             <div className="px-80 py-20 space-y-12">
+                <h2 className="font-semibold leading-7 text-3xl font-bold hover:underline">Suas configura&ccedil;&otilde;es</h2>
                 <div className="border-b border-gray-900/10 pb-12">
                     <h2 className="text-base font-semibold leading-7 ">Perfil</h2>
                     <p className="mt-1 text-sm leading-6 ">
-                        Essas informações serão exibidas publicamente, portanto, tenha cuidado com o que você compartilha.
+                        Essas informa&ccedil;&otilde;es ser&atilde;o exibidas publicamente, portanto, tenha cuidado com o que voc&ecirc; compartilha.
                     </p>
 
                     <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
@@ -25,7 +33,7 @@ export default function FormConfig() {
                                     defaultValue={''}
                                 />
                             </div>
-                            <p className="mt-3 text-sm leading-6 ">Escreva sobre você...</p>
+                            <p className="mt-3 text-sm leading-6 ">Escreva sobre voc&ecirc;...</p>
                         </div>
 
                         <div className="col-span-full">
@@ -38,14 +46,14 @@ export default function FormConfig() {
                                     type="button"
                                     className="rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-black shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
                                 >
-                                    Change
+                                    Alterar
                                 </button>
                             </div>
                         </div>
 
                         <div className="col-span-full">
                             <label htmlFor="cover-photo" className="block text-sm font-medium leading-6 ">
-                                Cover photo
+                                Foto de capa
                             </label>
                             <div className="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
                                 <div className="text-center">
@@ -68,13 +76,13 @@ export default function FormConfig() {
                 </div>
 
                 <div className="border-b border-gray-900/10 pb-12">
-                    <h2 className="text-base font-semibold leading-7 ">Personal Information</h2>
-                    <p className="mt-1 text-sm leading-6 ">Use a permanent address where you can receive mail.</p>
+                    <h2 className="text-base font-semibold leading-7 ">Informa&ccedil;&otilde;es pessoais</h2>
+                    <p className="mt-1 text-sm leading-6 ">Use um endere&ccedil;o permanente onde voc&ecirc; pode receber e-mails.</p>
 
                     <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
                         <div className="sm:col-span-3">
                             <label htmlFor="first-name" className="block text-sm font-medium leading-6 ">
-                                First name
+                                Primeiro nome
                             </label>
                             <div className="mt-2">
                                 <input
@@ -89,7 +97,7 @@ export default function FormConfig() {
 
                         <div className="sm:col-span-3">
                             <label htmlFor="last-name" className="block text-sm font-medium leading-6 ">
-                                Last name
+                                &Uacute;ltimo nome
                             </label>
                             <div className="mt-2">
                                 <input
@@ -104,7 +112,7 @@ export default function FormConfig() {
 
                         <div className="sm:col-span-4">
                             <label htmlFor="email" className="block text-sm font-medium leading-6 ">
-                                Email address
+                                E-mail
                             </label>
                             <div className="mt-2">
                                 <input
@@ -119,7 +127,7 @@ export default function FormConfig() {
 
                         <div className="sm:col-span-3">
                             <label htmlFor="country" className="block text-sm font-medium leading-6 ">
-                                Country
+                                Pa&iacute;s
                             </label>
                             <div className="mt-2">
                                 <select
@@ -131,13 +139,14 @@ export default function FormConfig() {
                                     <option>United States</option>
                                     <option>Canada</option>
                                     <option>Mexico</option>
+                                    <option>Brasil</option>
                                 </select>
                             </div>
                         </div>
 
                         <div className="col-span-full">
                             <label htmlFor="street-address" className="block text-sm font-medium leading-6 ">
-                                Street address
+                                Endere&ccedil;o
                             </label>
                             <div className="mt-2">
                                 <input
@@ -152,7 +161,7 @@ export default function FormConfig() {
 
                         <div className="sm:col-span-2 sm:col-start-1">
                             <label htmlFor="city" className="block text-sm font-medium leading-6 ">
-                                City
+                                Cidade
                             </label>
                             <div className="mt-2">
                                 <input
@@ -167,7 +176,7 @@ export default function FormConfig() {
 
                         <div className="sm:col-span-2">
                             <label htmlFor="region" className="block text-sm font-medium leading-6 ">
-                                State / Province
+                                Estado
                             </label>
                             <div className="mt-2">
                                 <input
@@ -182,7 +191,7 @@ export default function FormConfig() {
 
                         <div className="sm:col-span-2">
                             <label htmlFor="postal-code" className="block text-sm font-medium leading-6 ">
-                                ZIP / Postal code
+                                CEP
                             </label>
                             <div className="mt-2">
                                 <input
@@ -198,14 +207,14 @@ export default function FormConfig() {
                 </div>
 
                 <div className="border-b border-gray-900/10 pb-12">
-                    <h2 className="text-base font-semibold leading-7 ">Notifications</h2>
+                    <h2 className="text-base font-semibold leading-7 ">Notifica&ccedil;&otilde;es</h2>
                     <p className="mt-1 text-sm leading-6 ">
-                        We'll always let you know about important changes, but you pick what else you want to hear about.
+                        Sempre informaremos sobre mudan&ccedil;as importantes, mas voc&ecirc; escolhe o que mais deseja receber.
                     </p>
 
                     <div className="mt-10 space-y-10">
                         <fieldset>
-                            <legend className="text-sm font-semibold leading-6 ">By Email</legend>
+                            <legend className="text-sm font-semibold leading-6 ">Por e-mail</legend>
                             <div className="mt-6 space-y-6">
                                 <div className="relative flex gap-x-3">
                                     <div className="flex h-6 items-center">
@@ -218,48 +227,16 @@ export default function FormConfig() {
                                     </div>
                                     <div className="text-sm leading-6">
                                         <label htmlFor="comments" className="font-medium ">
-                                            Comments
+                                            Promo&ccedil;&otilde;es
                                         </label>
-                                        <p className="text-gray-500">Get notified when someones posts a comment on a posting.</p>
-                                    </div>
-                                </div>
-                                <div className="relative flex gap-x-3">
-                                    <div className="flex h-6 items-center">
-                                        <input
-                                            id="candidates"
-                                            name="candidates"
-                                            type="checkbox"
-                                            className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
-                                        />
-                                    </div>
-                                    <div className="text-sm leading-6">
-                                        <label htmlFor="candidates" className="font-medium ">
-                                            Candidates
-                                        </label>
-                                        <p className="text-gray-500">Get notified when a candidate applies for a job.</p>
-                                    </div>
-                                </div>
-                                <div className="relative flex gap-x-3">
-                                    <div className="flex h-6 items-center">
-                                        <input
-                                            id="offers"
-                                            name="offers"
-                                            type="checkbox"
-                                            className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
-                                        />
-                                    </div>
-                                    <div className="text-sm leading-6">
-                                        <label htmlFor="offers" className="font-medium ">
-                                            Offers
-                                        </label>
-                                        <p className="text-gray-500">Get notified when a candidate accepts or rejects an offer.</p>
+                                        <p className="text-gray-500">Seja notificado quando houver promo&ccedil;&otilde;es.</p>
                                     </div>
                                 </div>
                             </div>
                         </fieldset>
                         <fieldset>
-                            <legend className="text-sm font-semibold leading-6 ">Push Notifications</legend>
-                            <p className="mt-1 text-sm leading-6 ">These are delivered via SMS to your mobile phone.</p>
+                            <legend className="text-sm font-semibold leading-6 ">Por SMS</legend>
+                            <p className="mt-1 text-sm leading-6 ">Estes s&atilde;o entregues via SMS no seu telefone celular.</p>
                             <div className="mt-6 space-y-6">
                                 <div className="flex items-center gap-x-3">
                                     <input
@@ -269,7 +246,7 @@ export default function FormConfig() {
                                         className="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600"
                                     />
                                     <label htmlFor="push-everything" className="block text-sm font-medium leading-6 ">
-                                        Everything
+                                        Tudo
                                     </label>
                                 </div>
                                 <div className="flex items-center gap-x-3">
@@ -280,7 +257,7 @@ export default function FormConfig() {
                                         className="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600"
                                     />
                                     <label htmlFor="push-email" className="block text-sm font-medium leading-6 ">
-                                        Same as email
+                                        O mesmo que e-mail
                                     </label>
                                 </div>
                                 <div className="flex items-center gap-x-3">
@@ -291,7 +268,7 @@ export default function FormConfig() {
                                         className="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600"
                                     />
                                     <label htmlFor="push-nothing" className="block text-sm font-medium leading-6 ">
-                                        No push notifications
+                                        Sem notifica&ccedil;&otilde;es
                                     </label>
                                 </div>
                             </div>
@@ -301,14 +278,14 @@ export default function FormConfig() {
             </div>
 
             <div className="mt-6 flex items-center justify-end gap-x-6">
-                <button type="button" className="text-sm font-semibold leading-6 ">
-                    Cancel
+                <button type="button" className="text-sm font-semibold leading-6" onClick={ handleClickCancel }>
+                    Cancelar
                 </button>
                 <button
                     type="submit"
                     className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                 >
-                    Save
+                    Salvar
                 </button>
             </div>
         </form>
