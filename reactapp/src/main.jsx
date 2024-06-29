a94454ae40bb8911a0080ec2df9efb4fe4a102d2
sammsts <samuelmacielsts@gmail.com>
2024-06-29 17:33:18 -0300

    Fix: Import react-dom/client

--- reactapp/src/main.jsx
+++ reactapp/src/main.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import './index.css';
 import StackedLayout from './ui/components/stackedlayout/StackedLayout.jsx';
