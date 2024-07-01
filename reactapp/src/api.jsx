8df4de1d9eb8841d3929354d3bb753075bc99d9d
sammsts <samuelmacielsts@gmail.com>
2024-06-30 22:37:04 -0300

    Feat: Deploy azure

--- reactapp/src/api.jsx
+++ reactapp/src/api.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://localhost:7063/api',
+    baseURL: 'https://webapiecommerceengsoft2.azurewebsites.net/api',
     headers: {
         Authorization: `Bearer ${localStorage.getItem('tokenJWT')}`,
     },
